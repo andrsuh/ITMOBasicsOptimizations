ba7336799baa3567da138d60e19a9c617c99d4e4
andrsuh <andrsuh@rambler.ru>
2016-06-16 21:25:17 +0300

    initial commit

---
+++ src/Oracle.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.lang.Math.pow;
+
+public class Oracle {
+    private int counter = (int) 10E4;
+
+    public static void main(String[] args) {
+        Oracle oracle = new Oracle();
+
+        Double minValue = null;
+        Double argValue = null;
+
+        for (double i = -10; i < 10; i += 0.1) {
+            double next = oracle.quality(i);
+            System.out.println("F(x) = " + next + " x = " + i);
+
+            if (minValue == null || next < minValue) {
+                minValue = next;
+                argValue = (double) i;
+            }
+        }
+
+        System.out.println("Function min == " + minValue);
+        System.out.println("Argument min == " + argValue);
+    }
+
+    public Double quality(double x) {
+        if (counter-- == 0) {
+            return null;
+        }
+
+        List<Double> values = new ArrayList<Double>() {{
+            add(1 + 2 * pow((x - 1), 2));
+            add(2 + 1.8 * pow((x - 2), 2));
+            add(3 + 1.6 * pow((x - 3), 2));
+            add(4 + 1.4 * pow((x - 4), 2));
+            add(5 + 1.2 * pow((x - 5), 2));
+        }};
+
+        return Collections.min(values);
+    }
+}
---
+++ src/RandomSearch.java
@@ -0,0 +1,44 @@
+public class RandomSearch {
+    private double step = 0.001;
+    private Oracle oracle = new Oracle();
+
+    public static void main(String[] args) {
+        RandomSearch s = new RandomSearch();
+        System.out.println("Optimum " + s.searchOptimum());
+    }
+
+    public Double searchOptimum() {
+        Double globalOptimum = null;
+        double globalOptimumQuality = Double.POSITIVE_INFINITY;
+        while (true) {
+            double currentSolution = Math.random() * 20 - 10; // [-10, 10)
+            double currentQuality = oracle.quality(currentSolution);
+
+            if (oracle.quality(currentSolution + step) > oracle.quality(currentSolution)) {
+                step *= -1;
+            }
+
+            while (true) {
+                double pretendentSolution = currentSolution + step;
+                Double pretendentQuality = oracle.quality(pretendentSolution);
+
+                if (pretendentQuality == null) {
+                    return globalOptimum;
+                }
+
+                if (pretendentQuality < currentQuality) { // we are looking for minimum
+                    System.out.println("CurSolution: " + currentSolution + " CurQuality: " + currentQuality);
+                    System.out.println("----------------------------------------------------------");
+                    currentSolution = pretendentSolution;
+                    currentQuality = pretendentQuality;
+                } else {
+                    if (currentQuality < globalOptimumQuality) {
+                        globalOptimum = currentSolution;
+                        globalOptimumQuality = currentQuality;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
