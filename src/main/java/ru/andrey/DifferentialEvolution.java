bd74a27981b2932e1ee013a1f8b468cb0b3f1f97
Sukhovitsky, Andrey <Andrey.Sukhovitsky@emc.com>
2016-09-29 15:57:10 +0300

    remove brute force parameter selection

--- src/main/java/ru/andrey/DifferentialEvolution.java
+++ src/main/java/ru/andrey/DifferentialEvolution.java
@@ -4,50 +4,32 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static javafx.scene.input.KeyCode.F;
-
 public class DifferentialEvolution extends OptimizationMethod {
-    private static double DIFFEVOLUTION_PARAMETER;
-    private static double MUTATION_PROBABILITY;
-
+    private static final double DIFFEVOLUTION_PARAMETER = 0.05;
+    private static final double MUTATION_PROBABILITY = 0.2;
+    private static final int POPULATION_SIZE = 1000;
 
     private int populationSize;
-
-    public static void main(String[] args) {
-        double acc = 0.0;
-        DifferentialEvolution s = new DifferentialEvolution(10, 1000);
-
-        Double bestMutationProbability = null;
-        Double bestDiffEvParameter = null;
-        Double bestAverage = null;
-
-        for (double diffParam = 0.05; diffParam < 2.0; diffParam += 0.2) {
-            DIFFEVOLUTION_PARAMETER = diffParam;
-            for (double prob = 0.0; prob < 1.0; prob += 0.1) {
-                for (int i = 0; i < 10; ++i) {
-                    double diff = s.getDifference();
-                    acc += diff;
-                    System.out.println("Difference " + diff);
-                }
-                if (bestAverage == null || acc / 10.0 < bestAverage) {
-                    bestAverage = acc / 10.0;
-                    bestMutationProbability = prob;
-                    bestDiffEvParameter = diffParam;
-                }
-            }
-        }
-        System.out.println("best param = " + bestDiffEvParameter + " best prob = " + bestMutationProbability);
-    }
 
     public DifferentialEvolution(int dimension, int populationSize) {
         super(dimension);
         this.populationSize = populationSize;
     }
 
+    public static void main(String[] args) {
+        double acc = 0.0;
+        DifferentialEvolution s = new DifferentialEvolution(10, POPULATION_SIZE);
+        for (int i = 0; i < 10; ++i) {
+            double diff = s.getDifference();
+            acc += diff;
+            System.out.println("Difference " + diff);
+        }
+        System.out.println("Average = " + acc / 10.0);
+    }
+
     @Override
     public Solution searchOptimum() {
         oracle = new Oracle(dimension);
-//        int count = 0;
 
         List<Solution> population = new ArrayList<>();
         for (int i = 0; i < populationSize; ++i) {
@@ -56,11 +38,11 @@ public class DifferentialEvolution extends OptimizationMethod {
 
         Solution bestSolution = Solution.copyOf(population.get(0));
 
-        while(true) {
+        while (true) {
             for (int i = 0; i < populationSize; i++) {
                 Solution current = population.get(i);
 
-                int aIndex = randomIndexFromPopulation( populationSize, Arrays.asList(i));
+                int aIndex = randomIndexFromPopulation(populationSize, Arrays.asList(i));
                 int bIndex = randomIndexFromPopulation(populationSize, Arrays.asList(i, aIndex));
                 int cIndex = randomIndexFromPopulation(populationSize, Arrays.asList(i, aIndex, bIndex));
 
@@ -72,13 +54,7 @@ public class DifferentialEvolution extends OptimizationMethod {
 
                 double[] newSolution = current.getSolution();
 
-//                double F = (Math.random() > 0.1) ? 0.5 : 0.005;
-
                 for (int j = 0; j < dimension; j++) {
-//                    if (b[j] - c[j] > 0.1) {
-//                        System.out.println(b[j] - c[j]);
-//                    }
-
                     if (j == k || Math.random() > MUTATION_PROBABILITY) {
                         newSolution[j] = a[j] + DIFFEVOLUTION_PARAMETER * (b[j] - c[j]);
                     }
@@ -104,7 +80,7 @@ public class DifferentialEvolution extends OptimizationMethod {
     private int randomIndexFromPopulation(int to, List<Integer> except) {
         int index = except.get(0);
 
-        while(except.contains(index)) {
+        while (except.contains(index)) {
             index = (int) (Math.random() * to);
         }
 
